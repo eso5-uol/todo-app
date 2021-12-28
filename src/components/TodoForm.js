3d764e099fe1a08f9253a77f269a8c44ca3680dc
eso5-uol <60901246+eso5-uol@users.noreply.github.com>
2021-12-28 22:23:33 +0400

    Add files via upload

---
+++ src/components/TodoForm.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useRef} from 'react';
+
+
+
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
+  const inputRef = useRef(null)
+
+  useEffect (() => {
+      inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+      setInput(e.target.value);
+  };
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+        id: Math.floor(Math.random()*1000),
+        text: input
+    });
+
+    setInput('');
+  };
+
+  return (
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add a todo"
+          value={input}
+          name="text"
+          className="todo-input"
+          onChange={handleChange}
+          ref={inputRef}
+        ></input>
+        <button onClick={handleSubmit} className="todo-button">Update</button>
+      </form>
+  )
+}
+
+export default TodoForm
